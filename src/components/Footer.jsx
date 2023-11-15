05dcfddee02d6b01ba3e3c1a3c9275747a1a3a56
Jeronimo Daniel Itzcovich <jditzco@hotmail.com>
2023-11-15 20:25:04 -0300

    sa

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer>
             <img alt='Footer image' src='/favicon.jpg' />
-            <p>Hecho con <span className="heart">❤</span> por Jero con axel
+            <p>CSS Hecho con <span className="heart">❤</span> por Jero con axel
             </p>
         </footer>
     )
